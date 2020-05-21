5f3d25fede5dfe5a3ce77ecd75bdb2f16d49fbe7
rakeshlohar786 <rakeshlohar@live.com>
2020-05-21 22:38:48 +0530

    Initial Project Commit

---
+++ src/main/java/com/ex/rkpetclinic/RkPetClinicApplication.java
@@ -0,0 +1,13 @@
+package com.ex.rkpetclinic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RkPetClinicApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RkPetClinicApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/ex/rkpetclinic/RkPetClinicApplicationTests.java
@@ -0,0 +1,16 @@
+package com.ex.rkpetclinic;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class RkPetClinicApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
