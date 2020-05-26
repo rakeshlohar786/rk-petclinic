4a8eff5dd35809358a0dc58e98c08507c03992ed
rakeshlohar786 <rakeshlohar@live.com>
2020-05-26 18:11:11 +0530

    Creating interfaces for the pet clinic services. closes #5

---
+++ rk-petclinic-jpa/src/main/java/rkpetclinic/service/OwnerService.java
@@ -0,0 +1,20 @@
+package rkpetclinic.service;
+
+import rkpetclinic.model.Owner;
+
+import java.util.Set;
+
+public interface OwnerService {
+
+    Owner findByLastName(String lastName);
+
+    Owner findById(Long id);
+
+    Owner save(Owner owner);
+
+    Set<Owner> findAll();
+
+
+
+
+}
---
+++ rk-petclinic-jpa/src/main/java/rkpetclinic/service/VetService.java
@@ -0,0 +1,15 @@
+package rkpetclinic.service;
+
+import rkpetclinic.model.Vet;
+
+import java.util.Set;
+
+public interface VetService {
+
+    Vet findById(Long id);
+
+    Vet save(Vet vet);
+
+    Set<Vet> findAll();
+
+}
