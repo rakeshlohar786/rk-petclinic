45e9de63efc043e3dbb961dcdd9a0bb782498475
rakeshlohar786 <rakeshlohar@live.com>
2020-05-31 19:13:37 +0530

    Listed vets on index pages. closes #15

--- rk-petclinic-web/src/main/java/com/ex/rkpetclinic/controller/VetController.java
+++ rk-petclinic-web/src/main/java/com/ex/rkpetclinic/controller/VetController.java
@@ -1,14 +1,23 @@
 package com.ex.rkpetclinic.controller;
 
+import com.ex.rkpetclinic.service.VetService;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class VetController {
 
-        @RequestMapping({"/vets","/vets/index","/vets/index.html"})
-        public String getVetList(){
+        private final VetService vetService;
 
+    public VetController(VetService vetService) {
+        this.vetService = vetService;
+    }
+
+    @RequestMapping({"/vets","/vets/index","/vets/index.html"})
+        public String getVetList(Model model){
+
+            model.addAttribute("vets",vetService.findAll());
             return "vets/index";
         }
 
