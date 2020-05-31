e8d86dcf82320d0020acd3fa8c4e18952508ccf1
rakeshlohar786 <rakeshlohar@live.com>
2020-05-31 20:22:43 +0530

    Update Map Impl to manage setting of ID property . closes #23

--- rk-petclinic-jpa/src/main/java/com/ex/rkpetclinic/service/map/AbstractMapService.java
+++ rk-petclinic-jpa/src/main/java/com/ex/rkpetclinic/service/map/AbstractMapService.java
@@ -1,13 +1,12 @@
 package com.ex.rkpetclinic.service.map;
 
-import java.util.HashMap;
-import java.util.HashSet;
-import java.util.Map;
-import java.util.Set;
+import com.ex.rkpetclinic.model.BaseEntity;
 
-public abstract class AbstractMapService<T,ID> {
+import java.util.*;
 
-    protected Map<ID,T> map = new HashMap();
+public abstract class AbstractMapService<T extends BaseEntity,ID extends Long> {
+
+    protected Map<Long,T> map = new HashMap();
 
     Set<T> findAll(){
         return new HashSet<>(map.values());
@@ -17,9 +16,18 @@ public abstract class AbstractMapService<T,ID> {
         return map.get(id);
     }
 
-    T save(ID id,T object){
-       map.put(id,object);
-        return  object;
+    T save(T object){
+
+        if(object != null){
+            if(object.getId() == null){
+                object.setId(getNextId());
+            }
+            map.put(object.getId(),object);
+        }else{
+            throw new RuntimeException("Object can no be null.");
+        }
+
+     return  object;
     }
 
     void deleteById(ID id){
@@ -29,6 +37,18 @@ public abstract class AbstractMapService<T,ID> {
     void delete(T object){
 
         map.entrySet().removeIf(entry -> entry.getValue().equals(object));
+    }
+
+    public Long getNextId(){
+
+        Long nextId = null;
+        try {
+            nextId = Collections.max(map.keySet()) + 1;
+        }catch (NoSuchElementException e){
+            nextId = 1L;
+            e.printStackTrace();
+        }
+        return nextId;
     }
 
 }
--- rk-petclinic-web/src/main/java/com/ex/rkpetclinic/bootstrap/DataLoader.java
+++ rk-petclinic-web/src/main/java/com/ex/rkpetclinic/bootstrap/DataLoader.java
@@ -23,13 +23,11 @@ public class DataLoader implements CommandLineRunner {
     public void run(String... args) throws Exception {
 
         Owner owner1 = new Owner();
-        owner1.setId(1L);
         owner1.setFirstName("Rakesh");
         owner1.setSecondName("Lohar");
         ownerService.save(owner1);
 
         Owner owner2 = new Owner();
-        owner2.setId(2L);
         owner2.setFirstName("Lalit");
         owner2.setSecondName("Desai");
         ownerService.save(owner2);
@@ -37,13 +35,11 @@ public class DataLoader implements CommandLineRunner {
         System.out.println("Owner Data Loaded...........");
 
         Vet vet1 = new Vet();
-        vet1.setId(1L);
         vet1.setFirstName("Jay");
         vet1.setSecondName("Pandey");
         vetService.save(vet1);
 
         Vet vet2 = new Vet();
-        vet2.setId(2L);
         vet2.setFirstName("Ivyanka");
         vet2.setSecondName("Trump");
         vetService.save(vet2);
