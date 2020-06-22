1d78eb545c9f10660fdec721c3846e3bc6ea96de
rakeshlohar786 <rakeshlohar@live.com>
2020-06-22 13:08:15 +0530

    Added PetService and PetServiceMap . closes #29

---
+++ rk-petclinic-jpa/src/main/java/com/ex/rkpetclinic/service/PetTypeService.java
@@ -0,0 +1,8 @@
+package com.ex.rkpetclinic.service;
+
+import com.ex.rkpetclinic.model.PetType;
+import org.springframework.data.repository.CrudRepository;
+
+public interface PetTypeService extends CrudService<PetType,Long> {
+
+}
---
+++ rk-petclinic-jpa/src/main/java/com/ex/rkpetclinic/service/map/PetTypeMapService.java
@@ -0,0 +1,36 @@
+package com.ex.rkpetclinic.service.map;
+
+import com.ex.rkpetclinic.model.PetType;
+import com.ex.rkpetclinic.service.PetTypeService;
+
+import java.util.Optional;
+import java.util.Set;
+
+public class PetTypeMapService extends AbstractMapService<PetType,Long> implements PetTypeService
+{
+    @Override
+    public Set<PetType> findAll() {
+        return super.findAll();
+    }
+
+    @Override
+    public PetType findById(Long id) {
+        return super.findById(id);
+    }
+
+    @Override
+    public PetType save(PetType object) {
+        return super.save(object);
+    }
+
+    @Override
+    public void delete(PetType object) {
+         super.delete(object);
+    }
+
+    @Override
+    public void deleteById(Long id) {
+        super.deleteById(id);
+    }
+}
+
