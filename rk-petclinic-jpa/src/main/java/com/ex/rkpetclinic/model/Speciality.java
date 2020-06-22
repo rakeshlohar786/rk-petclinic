049de3c1b04c8e8c3f582a1b70d6117258925fa5
rakeshlohar786 <rakeshlohar@live.com>
2020-06-22 11:18:43 +0530

    Added the speciality entity. Associated vet with speciality. closes #27

---
+++ rk-petclinic-jpa/src/main/java/com/ex/rkpetclinic/model/Speciality.java
@@ -0,0 +1,14 @@
+package com.ex.rkpetclinic.model;
+
+public class Speciality extends BaseEntity {
+
+    private String decription;
+
+    public String getDecription() {
+        return decription;
+    }
+
+    public void setDecription(String decription) {
+        this.decription = decription;
+    }
+}
